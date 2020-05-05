872e9574146b40cb17585e914b6667c8eac55daa
Anupam Dung Dung <anupam-d2@live.com>
2020-05-06 00:55:53 +0530

    First commit-Task0

---
+++ Task0/scriptground.js
@@ -0,0 +1,15 @@
+// alert("Connected")
+
+
+var stairs=document.querySelector('#up1')
+stairs.style.cursor="pointer"
+stairs.addEventListener('click',()=>{
+    window.location.href="./first_floor.html"
+})
+
+var stairs=document.querySelector('#up2')
+stairs.style.cursor="pointer"
+stairs.addEventListener('click',()=>{
+    window.location.href="./first_floor.html"
+})
+
---
+++ Task0/scriptsecond.js
@@ -0,0 +1,14 @@
+
+
+var stairs2=document.querySelector('#down1')
+stairs2.style.cursor="pointer"
+stairs2.addEventListener('click',()=>{
+    window.location.href="./first_floor.html"
+})
+
+
+var stairs2=document.querySelector('#down2')
+stairs2.style.cursor="pointer"
+stairs2.addEventListener('click',()=>{
+    window.location.href="./first_floor.html"
+})
